6073d6ba2818dc58dc15f62109b7b57277045547
Anya Singh <anyasingh070605@gmail.com>
2022-05-13 06:39:59 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"joe-dark-blue": "#202143",
+				"joe-dark-blue": "#c53030",
 				"joe-light-blue": "#30325B",
-				"joe-purple": "#7E84FF",
+				"joe-purple": "#dd6b20",
 			},
 		},
 	},
